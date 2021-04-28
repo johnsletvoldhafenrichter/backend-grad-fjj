febf9275e3c316ee39000dd74e470b0e3ec6449a
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2021-04-28 18:18:02 +0200

    ?????????????

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 
+
+console.log("?????????")
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
