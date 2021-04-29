dcb3c17be771dc92281bbd28904b00a93897d735
GitJarand <79191936+GitJarand@users.noreply.github.com>
2021-04-29 17:03:24 +0200

    john debugging everywhere

--- src/index.js
+++ src/index.js
@@ -23,17 +23,29 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 })
 
+<<<<<<< Updated upstream
 app.post('/session', async (req, res) => {
+=======
+app.post('/session', async (req, res, next) => {
+>>>>>>> Stashed changes
   const {userName, password} = req.body;
   try {
     const user = await getUserByUserName(userName);
 
     if (!user) {
+<<<<<<< Updated upstream
       return res.status(401).send({error: 'Unknown user'});
     }
 
     if (user.password !== password) {
       return res.status(401).send({error: 'Wrong password'});
+=======
+      return res.status(401).send('Unknown user');
+    }
+
+    if (user.password !== password) {
+      return res.status(401).send('Wrong password');
+>>>>>>> Stashed changes
     }
 
     const token = jwt.sign({
@@ -42,8 +54,14 @@ app.post('/session', async (req, res) => {
     }, secret);
 
     res.send({token});
+<<<<<<< Updated upstream
   } catch (error) {
     res.status(500).send({error: error.message});
+=======
+  } catch
+    (error) {
+    next(error);
+>>>>>>> Stashed changes
   }
 });
 
@@ -51,6 +69,7 @@ app.get('/authenticate', authenticate, (req, res) => {
   res.status(200).send("OK");
 });
 
+<<<<<<< Updated upstream
 app.get('/courses', authenticate, async (req, res) => {
     const courses = await getALlCourses();
     res.send(courses);
@@ -60,6 +79,35 @@ app.post('/profile', authenticate, async (req, res) => {
   const {userId} = req.body;
   const userProfile = await getUserProfile(userId);
   res.send(userProfile);
+=======
+app.get('/courses', authenticate, async (req, res, next) => {
+  try {
+    const courses = await getALlCourses();
+    if (!courses) {
+      res.status(404).send('Nothing found in Database!')
+      return;
+    }
+    res.send(courses);
+  } catch
+    (error) {
+    next(error);
+  }
+})
+;
+
+app.post('/profile', authenticate, async (req, res, next) => {
+  try {
+    const {userId} = req.body;
+    const userProfile = await getUserProfile(userId);
+    if (!userProfile) {
+      res.status(404).send('Nothing found in Database!')
+      return;
+    }
+    res.send(userProfile);
+  } catch (error) {
+    next(error);
+  }
+>>>>>>> Stashed changes
 })
 
 // start the Express server
