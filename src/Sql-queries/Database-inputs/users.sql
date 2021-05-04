06a493695e2f3609df6ed747510facc7cf18865d
tunestam <80259169+tunestam@users.noreply.github.com>
2021-05-04 18:39:09 +0200

    Updated inputs in users
    Added prolfile picture to Marte Nordnes

--- src/Sql-queries/Database-inputs/users.sql
+++ src/Sql-queries/Database-inputs/users.sql
@@ -21,12 +21,12 @@
 
 
 -- Userbase
-INSERT INTO users (first_name, last_name, phone_number, email, password, position_id, location_id, specialization_id, user_name)
-VALUES ('Marthe', 'Nordnes', 42682942, 'marthe@nordnes.no', 'passord123', 2, 1, 1, 'marthenordnes'),
-       ('Marcus', 'Kleiven', 12121212 , 'marcus@kleiven.no', '123', 1, 10, 9, 'marcuskleiven'),
-       ('Ola', 'Nordmann', 12341234, 'ola@nordmann.se', 'abc', 6, 9, 15, 'olanordmann'),
-       ('Kari', 'Nordmann', 43214321, 'kari@nordmann.se', 'def', 2, 6, 8, 'karinordmann'),
-       ('Dina', 'Medeiros', 12345678, 'dina@medeiros.com', 'qwerty', 2, 8, 7, 'dina'),
+INSERT INTO users (first_name, last_name, phone_number, email, password, position_id, location_id, specialization_id, user_name, profile_picture)
+VALUES ('Marthe', 'Nordnes', 42682942, 'marthe@nordnes.no', 'passord123', 2, 1, 1, 'marthenordnes', 'https://www.vid.no/site/assets/files/11206/bachelor-i-sykepleie-vid.1080x1080.jpg'),
+       ('Marcus', 'Kleiven', 12121212 , 'marcus@kleiven.no', '123', 1, 10, 9, 'marcuskleiven', null),
+       ('Ola', 'Nordmann', 12341234, 'ola@nordmann.se', 'abc', 6, 9, 15, 'olanordmann', null),
+       ('Kari', 'Nordmann', 43214321, 'kari@nordmann.se', 'def', 2, 6, 8, 'karinordmann', null),
+       ('Dina', 'Medeiros', 12345678, 'dina@medeiros.com', 'qwerty', 2, 8, 7, 'dina', null),
        ('a', 'a', 1, 'a', 'a', 1, 1, 1, 'a')
        ;
 
